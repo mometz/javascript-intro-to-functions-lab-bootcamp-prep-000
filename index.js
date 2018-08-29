828534cf7ac10bfd5a69773c478f12e8f7992222
Moritz Muehlhausen <moritz.muehlhausen@gmail.com>
2018-08-29 19:13:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logWhisper(string) {
 } 
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase) {
+  if (string.toLowerCase === string ) {
     return `I can't hear you!`
   } else if (string.toUpperCase) {
     return `YES INDEED!`
