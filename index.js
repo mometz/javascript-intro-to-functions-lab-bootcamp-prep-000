9c489f3daa44be573a9c166419cf2227e4a24313
Moritz Muehlhausen <moritz.muehlhausen@gmail.com>
2018-08-29 18:48:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-var uppercase = "HELLO!"
- 
-uppercase.toUpperCase() === uppercase
- 
-var lowercase = 'hello!'
- 
-lowercase.toLowerCase() === lowercase
- 
-var mixedCase = 'Hi there!'
- 
-mixedCase.toLowerCase() === mixedCase 
- 
-mixedCase.toUpperCase() === mixedCase
-
-
-
-
 function shout(string) {
   return string.toUpperCase()
 }
